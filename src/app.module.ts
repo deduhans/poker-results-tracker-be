35ae47969b8062fb77790a2816893d8a50f9b4a1
Serhii Iliushenko <deduhan@serhiis-macbook-air.home>
2025-03-04 20:47:55 +0100

    preparing

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ const ENV = process.env.NODE_ENV;
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `./env/.env.${ENV}`
+      envFilePath: `./env/.env.local`
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
